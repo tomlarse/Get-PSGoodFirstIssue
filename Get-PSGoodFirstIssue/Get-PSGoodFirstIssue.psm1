6c7e88be8f8ac5ff500200b2873df9638261b1fe
Tom-Inge Larsen <tom-inge.larsen@rewired.no>
2018-11-12 15:25:05 +0100

    Formatting update, thanks Staffan! https://www.youtube.com/watch?v=tbXozToIzto

--- Get-PSGoodFirstIssue/Get-PSGoodFirstIssue.psm1
+++ Get-PSGoodFirstIssue/Get-PSGoodFirstIssue.psm1
@@ -1,3 +1,17 @@
+Update-TypeData -TypeName PSGFI.GithubIssue -MemberType ScriptProperty -MemberName Repository -Value {"$(($this.repository_url -split '/')[-2])/$(($this.repository_url -split '/')[-1])" -replace '.*',"`e[92m`$0`e[39m"} -Force
+Update-TypeData -TypeName PSGFI.GithubIssue -MemberType ScriptProperty -MemberName "Title " -Value {$this.Title -replace '.*',"`e[96m`$0`e[39m"} -Force
+Update-TypeData -TypeName PSGFI.GithubIssue -MemberType ScriptProperty -MemberName Issue -Value {$this.number -replace '.*',"`e[92m`$0`e[39m"} -Force
+Update-TypeData -TypeName PSGFI.GithubIssue -MemberType ScriptProperty -MemberName Status -Value {$this.state -replace '.*',"`e[92m`$0`e[39m"} -Force
+Update-TypeData -TypeName PSGFI.GithubIssue -MemberType ScriptProperty -MemberName Link -Value {$this.html_url -replace '.*',"`e[96m`$0`e[39m"} -Force
+Update-TypeData -TypeName PSGFI.GithubIssue -MemberType ScriptProperty -MemberName "Assigned to" -Value {
+    if ($this.Assignee) {
+        $this.Assignee -replace '.*',"`e[92m`$0`e[39m"
+    } else {
+        "Unassigned" -replace '.*',"`e[92m`$0`e[39m"
+    }
+} -Force
+Update-TypeData -TypeName PSGFI.GithubIssue -DefaultDisplayPropertySet "Title ", Repository, Issue, Status, "Assigned to", Link
+
 function Get-PSGoodFirstIssue {
     [CmdletBinding()]
     param (
@@ -17,7 +31,11 @@ function Get-PSGoodFirstIssue {
             }
         }
 
-        Invoke-RestMethod "https://api.github.com/repos/$Repo/issues" -Body $irmbody -Headers $irmheader -FollowRelLink | ForEach-Object {$_} | Get-Random
+        $issue = Invoke-RestMethod "https://api.github.com/repos/$Repo/issues" -Body $irmbody -Headers $irmheader -FollowRelLink | ForEach-Object {$_} | Get-Random
+
+        $issue.pstypenames.insert(0,"PSGFI.GithubIssue")
+
+        $issue
     }
 }
 
