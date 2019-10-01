a51c68f905e1e888be819ea7b5fdf1c576533308
Wickham.Andrew.J <AndrewWickham@westfieldgrp.com>
2019-10-01 10:50:35 -0400

    Support PowerShell 5.1

--- Get-PSGoodFirstIssue/Get-PSGoodFirstIssue.psm1
+++ Get-PSGoodFirstIssue/Get-PSGoodFirstIssue.psm1
@@ -1,18 +1,60 @@
-Update-TypeData -TypeName PSGFI.GithubIssue -MemberType ScriptProperty -MemberName Repository -Value {"$(($this.repository_url -split '/')[-2])/$(($this.repository_url -split '/')[-1])" -replace '.*',"`e[92m`$0`e[39m"} -Force
-Update-TypeData -TypeName PSGFI.GithubIssue -MemberType ScriptProperty -MemberName "Title " -Value {$this.Title -replace '.*',"`e[96m`$0`e[39m"} -Force
-Update-TypeData -TypeName PSGFI.GithubIssue -MemberType ScriptProperty -MemberName Issue -Value {$this.number -replace '.*',"`e[92m`$0`e[39m"} -Force
-Update-TypeData -TypeName PSGFI.GithubIssue -MemberType ScriptProperty -MemberName Status -Value {$this.state -replace '.*',"`e[92m`$0`e[39m"} -Force
-Update-TypeData -TypeName PSGFI.GithubIssue -MemberType ScriptProperty -MemberName Link -Value {$this.html_url -replace '.*',"`e[96m`$0`e[39m"} -Force
+Update-TypeData -TypeName PSGFI.GithubIssue -MemberType ScriptProperty -MemberName Repository -Value {"$(($this.repository_url -split '/')[-2])/$(($this.repository_url -split '/')[-1])" -replace '.*',"$([char]0x1b)[92m`$0$([char]0x1b)[39m"} -Force
+Update-TypeData -TypeName PSGFI.GithubIssue -MemberType ScriptProperty -MemberName "Title " -Value {$this.Title -replace '.*',"$([char]0x1b)[96m`$0$([char]0x1b)[39m"} -Force
+Update-TypeData -TypeName PSGFI.GithubIssue -MemberType ScriptProperty -MemberName Issue -Value {$this.number -replace '.*',"$([char]0x1b)[92m`$0$([char]0x1b)[39m"} -Force
+Update-TypeData -TypeName PSGFI.GithubIssue -MemberType ScriptProperty -MemberName Status -Value {$this.state -replace '.*',"$([char]0x1b)[92m`$0$([char]0x1b)[39m"} -Force
+Update-TypeData -TypeName PSGFI.GithubIssue -MemberType ScriptProperty -MemberName Link -Value {$this.html_url -replace '.*',"$([char]0x1b)[96m`$0$([char]0x1b)[39m"} -Force
 Update-TypeData -TypeName PSGFI.GithubIssue -MemberType ScriptProperty -MemberName "Assigned to" -Value {
     if ($this.Assignee.login) {
-        $this.Assignee.login -replace '.*',"`e[92m`$0`e[39m"
+        $this.Assignee.login -replace '.*',"$([char]0x1b)[92m`$0$([char]0x1b)[39m"
     } elseif ($this.Assignee) {
-        $this.Assignee -replace '.*',"`e[92m`$0`e[39m"
+        $this.Assignee -replace '.*',"$([char]0x1b)[92m`$0$([char]0x1b)[39m"
     } else {
-        "Unassigned" -replace '.*',"`e[92m`$0`e[39m"
+        "Unassigned" -replace '.*',"$([char]0x1b)[92m`$0$([char]0x1b)[39m"
     }
 } -Force
 Update-TypeData -TypeName PSGFI.GithubIssue -DefaultDisplayPropertySet "Title ", Repository, Issue, Status, "Assigned to", Link
+
+# credit to fcabralpacheco for redirect url
+# https://github.com/PowerShell/PowerShell/issues/2896#issuecomment-365811411
+function Get-RedirectedUrl() {
+    param(
+      [Parameter(Mandatory = $true, Position = 0)]
+      [uri] $url,
+      [Parameter(Position = 1)]
+      [Microsoft.PowerShell.Commands.WebRequestSession] $session = $null
+    )
+
+    $request_url = $url
+    $retry = $false
+
+    do {
+      try {
+        $response = Invoke-WebRequest -Method Head -WebSession $session -Uri $request_url
+
+        if($null -ne $response.BaseResponse.ResponseUri)
+        {
+          # PowerShell 5
+          $result = $response.BaseResponse.ResponseUri.AbsoluteUri
+        } elseif ($null -ne $response.BaseResponse.RequestMessage.RequestUri) {
+          # PowerShell Core
+          $result = $response.BaseResponse.RequestMessage.RequestUri.AbsoluteUri
+        }
+
+        $retry = $false
+      } catch {
+        if(($_.Exception.GetType() -match "HttpResponseException") -and
+          ($_.Exception -match "302"))
+        {
+          $request_url = $_.Exception.Response.Headers.Location.AbsoluteUri
+          $retry = $true
+        } else {
+          throw $_
+        }
+      }
+    } while($retry)
+
+    return $result
+  }
 
 function Get-PSGoodFirstIssue {
     [CmdletBinding()]
@@ -33,7 +75,8 @@ function Get-PSGoodFirstIssue {
             }
         }
 
-        $issue = Invoke-RestMethod "https://api.github.com/repos/$Repo/issues" -Body $irmbody -Headers $irmheader -FollowRelLink | ForEach-Object {$_} | Get-Random
+        $url = Get-RedirectedUrl -Url "https://api.github.com/repos/$Repo/issues"
+        $issue = Invoke-RestMethod $url -Body $irmbody -Headers $irmheader | ForEach-Object {$_} | Get-Random
 
         $issue.pstypenames.insert(0,"PSGFI.GithubIssue")
 
@@ -64,7 +107,8 @@ function Get-PSHacktoberFestIssue {
 
         $uri = "https://api.github.com/search/issues?q=language:{0}+label:{1}+state:{2}" -f $Language, $Label, $State
 
-        $result = Invoke-RestMethod $uri -Body $irmbody -Headers $irmheader -FollowRelLink
+        $uri = Get-RedirectedUrl -Url $uri
+        $result = Invoke-RestMethod $uri -Body $irmbody -Headers $irmheader
         $issue = $result.items | Get-Random
         $issue.pstypenames.insert(0,"PSGFI.GithubIssue")
 
--- build.ps1
+++ build.ps1
@@ -1,3 +1,7 @@
+param (
+    $Task = 'Default'
+)
+
 function Resolve-Module {
     [Cmdletbinding()]
     param
@@ -45,5 +49,5 @@ Resolve-Module "Psake", "PSDeploy", "Pester", "BuildHelpers"
 
 Set-BuildEnvironment
 
-Invoke-psake .\psake.ps1
+Invoke-psake .\psake.ps1 -TaskList $Task
 exit ( [int]( -not $psake.build_success ) )
